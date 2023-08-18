00bb5e4bf493a899d8c42ad6e13a4bdd03b3465a
TianMing0409 <tianmingkoh@gmail.com>
2023-08-18 20:57:52 +0800

    Add project files.

---
+++ EtiqaAssessment/Models/Freelancer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EtiqaAssessment.Models
+{
+    public class Freelancer
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string username { get; set; }
+
+        public string email { get; set; }
+
+        public string phoneNo { get; set; }
+
+        public string skillsets { get; set; }
+
+        public string hobby { get; set; }
+    }
+}
---
+++ EtiqaAssessment/Models/FreelancersDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EtiqaAssessment.Models
+{
+    public class FreelancersDbContext: DbContext
+    {
+        public FreelancersDbContext(DbContextOptions<FreelancersDbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Freelancer> Freelancers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.; Initial Catalog=lbs; User Id=sa; password=123; TrustServerCertificate= True");
+        }
+    }
+}
